38a49c42f5037ba70803eca724b05fe08e341d41
jied <jied314@mail.sfsu.edu>
2015-04-07 12:03:45 -0700

    remove missing values with mode

--- Mini#2/remove_missing_values.py
+++ Mini#2/remove_missing_values.py
@@ -1,28 +1,20 @@
 import sys
 
 Missing_Value = '?'
-File_Size = 0
+Replace_dic = {1: 'Private', 6: 'Prof-specialty', 13: 'United-States'}
+files = ['in_data/adult.all.csv', 'in_data/adult.all1.csv']
 
 
 def main():
-    if len(sys.argv) is not 5:
-        print 'incorrect arguments\nneed: input_file1.txt input_file2.txt out_file1.txt out_file1.txt'
-        sys.exit(2)
-    else:
-        argv1 = sys.argv[1]
-        argv2 = sys.argv[2]
-        argv3 = sys.argv[3]
-        argv4 = sys.argv[4]
-
-    remove_missing_value(argv1, argv3)
-    remove_missing_value(argv2, argv4)
-    print "file size is: {0}".format(File_Size)
+    remove_missing_value()
 
 
-def remove_missing_value(input, output):
-    global File_Size
-    in_f = open(input, 'r')
-    out_f = open(output, 'w')
+def remove_missing_value():
+    in_f = open(files[0], 'r')
+    out_f = open(files[1], 'w')
+
+    target_indexes = Replace_dic.keys()
+
     while (1):
         line = in_f.readline()
         if line == '':  #EOF
@@ -32,14 +24,16 @@ def remove_missing_value(input, output):
         if line == '\r' or line == '\n':  #skip empty line
             continue
 
-        if not is_contain_missing_value(line):
-            out_f.write(line)
-            File_Size += 1
+        # separate words
+        ll = [x.strip() for x in line.split(',')]
 
+        # remove missing values
+        for i in range(0, len(ll) - 1):
+            if i in target_indexes and ll[i] == Missing_Value:
+                ll[i] = Replace_dic[i]
 
-def is_contain_missing_value(line):
-    return Missing_Value in line
-
+        out_f.write(','.join(ll))
+        out_f.write('\n')
 
 if __name__ == '__main__':
     main()
